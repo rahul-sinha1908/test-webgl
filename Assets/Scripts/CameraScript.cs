98776b631933efd3998227f5152b100e952b0bfd
Rahul Sinha <rahul.sinha1908@gmail.com>
2018-09-07 16:33:28 +0530

    added things like camera data fetching.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
 
+	private WebCamTexture mCamera = null;
+	public Material material;
+
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+	{
+			Debug.Log ("Script has been started");
+			
+			mCamera = new WebCamTexture ();
+			material.mainTexture = mCamera;
+			mCamera.Play ();
 	
-	// Update is called once per frame
-	void Update () {
-		
 	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
 }
